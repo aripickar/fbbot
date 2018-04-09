3549328eb2700737221554bfa4214139f3d235a7
aripickar <ari.pickar@gmail.com>
2018-04-08 21:14:05 -0700

    Add files via upload
    Bot at current time

---
+++ fbmessage.py
@@ -0,0 +1,24 @@
+import fbchat
+from fbchat import Client
+from fbchat.models import *
+import json
+key_file = 'keys.json'
+#Edit the keys.json file with your Username and Password
+with open(key_file) as f:
+    keys = json.load(f)
+client = Client(keys['username'], keys['password'])
+friends_file = 'friends.json'
+#add all freinds that want to be messaged to friends.json with the 
+#form "friendX" : "Friend name"
+with open(friends_file) as g:
+	friends = json.load(g)
+print(len(friends))
+for i in range(1, len(friends) + 1):
+	users = client.searchForUsers(friends['friend' + str(i)])
+	user = users[0]
+	print("User's ID: {}".format(user.uid))
+	print("User's name: {}".format(user.name))
+	print("User's profile picture url: {}".format(user.photo))
+	print("User's main url: {}".format(user.url))
+	client.send(Message(text= 'Hi ' + str(user.name).split()[0] + '! Vote for Will Wang at sa2018.vote/william'), thread_id=user.uid, thread_type=ThreadType.USER)
+client.logout()
