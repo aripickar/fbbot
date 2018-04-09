ad99bc64959316c29405a4d7b9c6e87e918bb6f8
aripickar <ari.pickar@gmail.com>
2018-04-09 02:31:19 -0700

    Update fbmessage.py

--- fbmessage.py
+++ fbmessage.py
@@ -22,10 +22,10 @@ for i in range(1, len(friends) + 1):
 	print("User's name: {}".format(user.name))
 	print("User's profile picture url: {}".format(user.photo))
 	print("User's main url: {}".format(user.url))
-	messages = ('Hi ' + str(user.name).split()[0] + '! Vote for ' + candidate + ' at asuc.org/elections',
-			'Hey ' + str(user.name).split()[0] + '! Its that time of year again, and I need you to vote for  ' + candidate + ' at asuc.org/elections',
+	messages = ('Hi ' + str(user.name).split()[0] + '! It is asuc elections time, and this year, I am encouraging people to vote for ' + candidate + ' at asuc.org/elections. You can read more about his platforms at studentaction.org! ',
+			'Hey ' + str(user.name).split()[0] + '! Its that time of year again, and I need you to vote for  ' + candidate + ' at asuc.org/elections!. It\'s really imoportant to me, and I would love if you could help!',
 			'What\'s up ' + str(user.name).split()[0] + '! It\'s voting season right now, and I need your help to get ' + candidate + ' elected. You can vote at asuc.org/elections. Thanks!',
-			'Hey ' + str(user.name).split()[0] + '! ASUC elections is really important to me, and I have been working to get ' + candidate + ' elected. You can help me out at asuc.org/elections',
-			'Hey ' + str(user.name) + '! ASUC elections are in full swing, and I\'m on the campaign of' + candidate + ', who is running for Senate. Would you mind taking 5 seconds and voting for them at asuc.org/elections')
+			'Hey ' + str(user.name).split()[0] + '! ASUC elections is really important to me, and I have been working to get ' + candidate + ' elected. You can help me out at asuc.org/elections. Thanks!',
+			'Hey ' + str(user.name) + '! ASUC elections are in full swing, and I\'m on the campaign of' + candidate + ', who is running for Senate. Would you mind taking 5 seconds and voting for them at asuc.org/elections? Thanks!')
 	client.send(Message(text= messages[random.randint(0,4)]), thread_id=user.uid, thread_type=ThreadType.USER)
 client.logout()
